33152e5fcf8adafad3319fba93f09882ae9b580f
gallexander <alexander.gallauner@gmail.com>
2017-03-16 17:18:39 +0100

    log2

---
+++ Source/log2/log2_builtin.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define LOG2(X) ((unsigned) (8*sizeof (unsigned long long) - __builtin_clzll((X)) - 1))
+
+double mytime(void){
+    struct timeval now;
+    gettimeofday(&now,NULL);
+    return (double) ((long long)now.tv_usec+(long long)now.tv_sec*1000000);
+}
+
+const int tab64[64] = {
+    63,  0, 58,  1, 59, 47, 53,  2,
+    60, 39, 48, 27, 54, 33, 42,  3,
+    61, 51, 37, 40, 49, 18, 28, 20,
+    55, 30, 34, 11, 43, 14, 22,  4,
+    62, 57, 46, 52, 38, 26, 32, 41,
+    50, 36, 17, 19, 29, 10, 13, 21,
+    56, 45, 25, 31, 35, 16,  9, 12,
+    44, 24, 15,  8, 23,  7,  6,  5};
+
+int log2_64(uint64_t value);
+
+int main(){
+    srand(time(NULL));
+    int i;
+    uint32_t x;
+    unsigned int result;
+    double time = mytime();
+    for (i = 0; i < pow(2,30); i++){
+        x = rand();
+        //result = log2_64(x);
+        result = LOG2(x);
+    }
+    time = mytime() -time;
+    printf("Time for log2: %f\n", time/1000000);
+    return 0;
+}
+
+int log2_64 (uint64_t value)
+{
+    value |= value >> 1;
+    value |= value >> 2;
+    value |= value >> 4;
+    value |= value >> 8;
+    value |= value >> 16;
+    value |= value >> 32;
+    return tab64[((uint64_t)((value - (value >> 1))*0x07EDD5E59A4E28C2)) >> 58];
+}
