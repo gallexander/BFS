65d24decbd4fd6c8887843f68fbc34ee7717ecbb
gallex <alexander.gallauner@gmail.com>
2017-06-02 11:43:56 +0200

    Hinzufügen von Bildern

--- tests.sh
+++ tests.sh
@@ -4,10 +4,10 @@ PATH=$PATH":"/opt/slurm/bin/
 module load mpi/openmpiS
 
 mpicc -Wall kronecker_generator_par.c -o gen -lm
-srun -p q_student -N 32 --ntasks-per-node=8 ./gen
+srun -p q_gallauner -N 32 --ntasks-per-node=16 ./gen
 
 mpicc -Wall bfs_seq.c -o bfs_seq -lm
-srun -p q_student -N 32 --ntasks-per-node=8 ./bfs_seq
+srun -p q_gallauner -N 32 --ntasks-per-node=16 ./bfs_seq
 
 #mpicc -Wall bfs_par_allvisited_parallelsort_improvement.c -o bfs_allreduce_normal -lm
 #srun -p q_student -N 32 --ntasks-per-node=1 ./bfs_allreduce_normal
@@ -33,12 +33,12 @@ srun -p q_student -N 32 --ntasks-per-node=8 ./bfs_seq
 #export OMP_NUM_THREADS=4
 #srun -p q_student -N 32 --ntasks-per-node=4 -c 4 ./bfs_hybrid
 
-mpicc -Wall bfs_par_alltoall.c -o bfs_alltoall -lm
-srun -p q_student -N 32 --ntasks-per-node=1 ./bfs_alltoall
-srun -p q_student -N 32 --ntasks-per-node=2 ./bfs_alltoall
-srun -p q_student -N 32 --ntasks-per-node=4 ./bfs_alltoall
-srun -p q_student -N 32 --ntasks-per-node=8 ./bfs_alltoall
-srun -p q_student -N 32 --ntasks-per-node=16 ./bfs_alltoall
+#mpicc -Wall bfs_par_alltoall.c -o bfs_alltoall -lm
+#srun -p q_student -N 32 --ntasks-per-node=1 ./bfs_alltoall
+#srun -p q_student -N 32 --ntasks-per-node=2 ./bfs_alltoall
+#srun -p q_student -N 32 --ntasks-per-node=4 ./bfs_alltoall
+#srun -p q_student -N 32 --ntasks-per-node=8 ./bfs_alltoall
+#srun -p q_student -N 32 --ntasks-per-node=16 ./bfs_alltoall
 
 #mpicc -Wall bfs_par_reducescatter.c -o bfs_reducescatter -lm
 #srun -p q_student -N 32 --ntasks-per-node=1 ./bfs_reducescatter
